6994d8399ef329ea7e872ad0189b2fb1d4149370
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-12-05 22:53:41 +0100

    Idea file commit

--- test.sql
+++ test.sql
@@ -4,5 +4,5 @@ DROP extension if exists url;
 CREATE extension url;
 CREATE TABLE table_with_url(my_url url);
 
-INSERT INTO table_with_url VALUES ('https://www.google.be/doc/?x#4');
+INSERT INTO table_with_url VALUES (url('https://www.google.be/doc/?x#4'));
 SELECT getauthority(my_url) FROM table_with_url;
