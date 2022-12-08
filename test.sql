f55ee2f32a84cb49451e1ea02034e352a5ce1a5b
jZong <jonathanstefanov1@gmail.com>
2022-12-08 18:09:57 +0100

    Finished all functions + types

--- test.sql
+++ test.sql
@@ -1,14 +1,21 @@
-DROP table if exists table_with_url;
-
-DROP extension if exists url CASCADE ;
-
+DROP table if exists urlnew;
+DROP EXTENSION IF EXISTS url CASCADE;
 CREATE extension url;
-CREATE TABLE table_with_url(my_url url);
 
-INSERT INTO table_with_url VALUES ('https://www.google.be/doc/?x#4');
-SELECT getport(my_url) FROM table_with_url;
-SELECT getprotocol(my_url) FROM table_with_url;
-SELECT getdefaultport(my_url) FROM table_with_url;
+
+CREATE TABLE urlnew(my_url url);
+CREATE INDEX am ON urlnew (my_url) WHERE equals(my_url, url_in('https://www.google.be/%27'));
+CREATE INDEX in2 ON urlnew (my_url);
+
+INSERT INTO urlnew VALUES ('https://olloooo@google.be:900/doc/?x#4'),
+       ('ftp://www.norse.be.super:4242/doc/?x#4%27'),
+       ('http://www.norse.be.super:4242/doc/?x#4%27'),
+       ('https://www.norse.be.super:4242/doc/#4%27'),
+       ('https://x.be/doc/?x#4%27'),
+       ('sambaaa://www.norse.be.super:4242/doc/?x#4');
+
+SELECT getdefaultport(my_url) FROM urlnew;
+SELECT getport(my_url) FROM urlnew;
 
 
 
