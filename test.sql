d587788faaf1c8cd0fce81274ca775f506bf1cd8
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-12-08 11:54:13 +0100

    Updating test.sql

--- test.sql
+++ test.sql
@@ -9,11 +9,6 @@ CREATE TABLE table_with_url
     my_url url
 );
 
-/* Really working ? :) */
-CREATE INDEX ra1 ON table_with_url (sameHost(url1 := table_with_url.my_url, url2 := table_with_url.my_url));
-CREATE INDEX ra2 ON table_with_url (samefile(url1 := table_with_url.my_url, url2 := table_with_url.my_url));
-CREATE INDEX ra3 ON table_with_url (equals(url1 := table_with_url.my_url, url2 := table_with_url.my_url));
-
 INSERT INTO table_with_url
 VALUES ('https://gaspardmertenpremierdunometavecuntreslongnom@www.norse.be.super:4242/doc/?x#4'),
        ('http://www.norse.be.super:4242/doc/?x#4'),
@@ -110,14 +105,36 @@ SELECT getscheme(my_url),
        getquery(my_url),
        getref(my_url)
 FROM table_with_url;
-VACUUM ANALYSE ;
 EXPLAIN ANALYSE SELECT equals(r.my_url, t.my_url)
 FROM table_with_url r
          LEFT OUTER JOIN  table_with_url t on true;
 
+
+DROP INDEX if exists ra1;
+DROP INDEX if exists ra2;
+DROP INDEX if exists ra3;
+DROP INDEX if exists ra;
+/* Really working ? :) */
+CREATE INDEX ra ON table_with_url (my_url);
+CREATE INDEX ra1 ON table_with_url (sameHost(url1 := my_url, url2 := table_with_url.my_url));
+CREATE INDEX ra2 ON table_with_url (samefile(url1 := my_url, url2 := table_with_url.my_url));
+CREATE INDEX ra3 ON table_with_url (equals(url1 := my_url, url2 := table_with_url.my_url));
+
+VACUUM ANALYSE ;
+
 EXPLAIN ANALYSE SELECT samehost(my_url, (Select my_url from table_with_url fetch first row only )) from table_with_url;
 EXPLAIN ANALYSE SELECT samefile(my_url, (Select my_url from table_with_url fetch first row only )) from table_with_url;
-EXPLAIN ANALYSE SELECT t1.my_url, twu.my_url from table_with_url t1 INNER JOIN table_with_url twu on t1.my_url = twu.my_url WHERE equals(t1.my_url,twu.my_url  );
 
 EXPLAIN ANALYSE SELECT r.my_url
 from table_with_url r;
+SELECT
+    tablename,
+    indexname,
+    indexdef
+FROM
+    pg_indexes
+WHERE
+        schemaname = 'public'
+ORDER BY
+    tablename,
+    indexname;
